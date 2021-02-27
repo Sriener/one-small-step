82c8a9b5e8ab2d594e887f9cdd49ec5a47d085b9
Steffen Riener <steffenr27@gmail.com>
2021-02-27 15:25:15 -0800

    Added numeral invalidation into while loop and uppercase so that all user input can be checked

--- romnum.py
+++ romnum.py
@@ -1,27 +1,7 @@
-# practice with dictionaries: month conversions and roman numerals
-
-month_conversions = {
-    "Jan": "January",
-    "Feb": "February",
-    "Mar": "March",
-    "Apr": "April",
-    "May": "May",
-    "Jun": "June",
-    "Jul": "July",
-    "Aug": "August",
-    "Sep": "September",
-    "Oct": "October",
-    "Nov": "November",
-    "Dec": "December"
-}
-
-# print(month_conversions["Mar"])
-# print(month_conversions.get("Sat", "Invalid key"))
-
+# Steffen "Ya Boi" Riener
 # Roman numeral calculator
-# no invalidation logic yet --
-#       you can put in any letter and it will try to calculate
-#       nonstandard numerals like IIIV would still be tested (needs to be fixed)
+# BUG -- nonstandard numerals like IIIV would still be tested
+
 roman_numerals = {
     "I": 1,
     "V": 5,
@@ -33,37 +13,48 @@ roman_numerals = {
 }
 
 # print(roman_numerals.get(test[x + 1], "Invalid numeral"))
-num = input("Enter Roman Numeral: ")
+num = input("Enter Roman Numeral: ").upper()
+
+# print(list(enumerate(num)))
 
 # size for how much to loop
 size = len(num)
-print("size is set to " + str(size))
+#print("size is set to " + str(size))
 
 # for total num equivalent to user numeral
 total = 0
-print("total is " + str(total))
+#print("total is " + str(total))
 
 # i for where loop is
 i = 0
-print("i is set to " + str(i))
+#print("i is set to " + str(i))
 
 # XXXIII
 while size > 0:
-    temp = roman_numerals.get(num[i], "Invalid roman numeral") # read index 0 of user string
-    # FIND WAY TO GET CONSOLE TO SAY INVALID AT ANY POINT IN THE NUMERAL
-    if i == len(num) - 1:
-        total += roman_numerals[num[i]]  # add to total
+    try:
+        temp = roman_numerals.get(num[i], "Invalid roman numeral") # read index 0 of user string
+        # FIND WAY TO GET CONSOLE TO SAY INVALID AT ANY POINT IN THE NUMERAL
+        if i == len(num) - 1:
+            total += roman_numerals[num[i]]  # add to total
+            break
+        elif temp < roman_numerals.get(num[i + 1], "Invalid roman numeral"): # CHECK IF NEXT is greater than index 0
+            total -= temp # subtract from total
+        else:
+            total += roman_numerals[num[i]] # add to total
+        size -= 1 # shift index up 1
+        i += 1
+        #print("total is now " + str(total))
+        #print("i is now " + str(i))
+        # repeat
+    except TypeError:
+        total = 0
+        print("Invalid Roman Numeral")
         break
-    elif temp < roman_numerals.get(num[i + 1], "Invalid roman numeral"): # CHECK IF NEXT is greater than index 0
-        total -= temp # subtract from total
-    else:
-        total += roman_numerals[num[i]] # add to total
-    size -= 1 # shift index up 1
-    i += 1
-    print("total is now " + str(total))
-    print("i is now " + str(i))
-    # repeat
 
-# Create a for loop -- it'll look and work better!
+# Create a for loop if you can!
+# Figure out how to use enumerate()
 
-print(num + " is " + str(total) + " in roman numerals.")
+if total > 0:
+    print(num + " is " + str(total) + " in roman numerals.")
+else:
+    pass
